456f57b9225c3e7131cdc05a21476e529718609d
Prometeus <evgeniy.jeka@gmail.com>
2020-12-31 19:07:10 +0200

    Gateway, API - place offer method in progress Project structure updated

--- gateway.py
+++ gateway.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask import request
 import logging
+import json
 from consumer_to_sql import ConsumerToSql
+from models.Offer import Offer
 
 logging.basicConfig(level=logging.INFO)
+
+# Start Kafka, Zoo Keeper, MySQL Server !
 
 # Initiating API Server
 app = Flask(__name__)
@@ -21,6 +25,7 @@ def place_offer():
     """
 
     """
+    verified_offer_params = ['owner_id', 'sum', 'duration', 'offered_interest']
 
     offer = request.get_json()
     logging.info(f"Offer received: {offer}")
@@ -30,7 +35,21 @@ def place_offer():
         # Once passed - create new Offer object and fill it with data received in the request
         # Use producer method to produce new kafka message - send Offer as JSON
 
-    return {"error": ".."}
+    if offer['type'] != 'offer':
+        return {"error": "Invalid object type for this API method"}
+
+    for param in verified_offer_params:
+        if param not in offer.keys():
+            return {"error": "Required parameter is missing in provided offer"}
+
+    # In future versions it is possible that the offer will be converted to Google Proto message
+    placed_offer = Offer(offer['owner_id'], offer['sum'], offer['duration'], offer['offered_interest'])
+    offer_to_producer = json.dumps(placed_offer.__dict__)
+
+    logging.info(offer_to_producer)
+    logging.info("Using Producer instance to send the offer to Kafka topic 'offers' ")
+
+    return {"result": ".."}
 #
 #
 # @app.route('/add_json/<action_type>', methods=['POST'])
