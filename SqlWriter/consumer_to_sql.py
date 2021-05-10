a0cf7f5122659ae73a747b141ff2b11bef2c9bc9
Prometeus <evgeniy.jeka@gmail.com>
2021-05-10 23:51:03 +0300

    SQL Writer - converted to separated micro service. Docker file added, config updated.
    The container built from service image runs in Docker network

--- SqlWriter/consumer_to_sql.py
+++ SqlWriter/consumer_to_sql.py
@@ -3,10 +3,11 @@ import json
 from kafka.admin import KafkaAdminClient, NewTopic
 import logging
 
-from SqlWriter.sql_writer import SqlWriter
+from sql_writer import SqlWriter
 from models.Offer import Offer
 from models.Bid import Bid
 from statuses import Types
+from local_config import Config
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,7 +26,7 @@ class ConsumerToSql(object):
 
     def start_consumer(self):
         # Creating Kafka topics or adding if the topic is missing
-        admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092", client_id='test')
+        admin_client = KafkaAdminClient(bootstrap_servers=Config.kafka_bootstrap_servers.value, client_id='test')
         existing_topics = admin_client.list_topics()
 
         required_topics = ("offers", "bids", "matches")
@@ -36,7 +37,7 @@ class ConsumerToSql(object):
         print(f"Existing topics: {admin_client.list_topics()}")
 
         # Initiating consumer
-        self.consumer = KafkaConsumer('offers', 'bids', 'matches', bootstrap_servers=['localhost:9092'],
+        self.consumer = KafkaConsumer('offers', 'bids', 'matches', bootstrap_servers=[Config.kafka_bootstrap_servers.value],
                                       auto_offset_reset='earliest', enable_auto_commit=True, group_id="sql_consumer")
 
 
