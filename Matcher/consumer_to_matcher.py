cdb712348b9b262d99a75908fd427bab4d2eb0f6
Prometeus <evgeniy.jeka@gmail.com>
2021-05-14 22:13:30 +0300

    Code fixes Docker file fixes

--- Matcher/consumer_to_matcher.py
+++ Matcher/consumer_to_matcher.py
@@ -24,20 +24,20 @@ logging.basicConfig(level=logging.INFO)
         #call producer_from_matcher.send and send the received Match object to 'matches' Kafka topic
 
 
-
-
 class ConsumerToMatcher(object):
 
     def __init__(self):
 
+        logging.info("ConsumerToMatcher: Creating MATCHER instance")
         self.matcher = Matcher()
+        self.consumer = None
 
         logging.info("ConsumerToMatcher: Verifying essential topics, starting main consumer")
         self.start_consumer()
-
         self.consume_process()
 
     def start_consumer(self):
+
         # Creating Kafka topics or adding if the topic is missing
         admin_client = KafkaAdminClient(bootstrap_servers=KafkaConfig.BOOTSTRAP_SERVERS.value, client_id='test')
         existing_topics = admin_client.list_topics()
@@ -52,7 +52,6 @@ class ConsumerToMatcher(object):
         # Initiating consumer
         self.consumer = KafkaConsumer('offers', 'bids', bootstrap_servers=[KafkaConfig.BOOTSTRAP_SERVERS.value],
                                     auto_offset_reset='earliest', enable_auto_commit=True, group_id="matcher_consumer")
-
 
     def consume_process(self):
         for msg in self.consumer:
@@ -74,8 +73,6 @@ class ConsumerToMatcher(object):
                                     object_content['status'])
 
                 self.matcher.add_offer(received_offer)
-
-
 
             elif object_content['type'] == Types.BID.value:
                 logging.info("ConsumerToMatcher: Processing BID")
