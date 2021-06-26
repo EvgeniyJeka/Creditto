5ba99fd89473aa1ff7fe403109fefcc3d0c92945
Prometeus <evgeniy.jeka@gmail.com>
2021-06-26 15:33:32 +0300

    SqlWriters - models are taken from package

--- SqlWriter/local_config.py
+++ SqlWriter/local_config.py
@@ -1,12 +1,5 @@
 from enum import Enum
 
-# class Config(Enum):
-#     sql_port = "creditto_cabin_db_1"
-#     sql_usr = 'root'
-#     sql_pwd = '123456'
-#     db_name = 'creditto'
-#     kafka_bootstrap_servers = "kafka:9092"
-
 class MatchingAlgorithm(Enum):
     BEST_OF_FIVE_LOWEST_INTEREST = 1
 
