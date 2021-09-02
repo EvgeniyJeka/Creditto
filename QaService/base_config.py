6e00dd3fb4ea25de09f5d1ba0495058a5831f571
Prometeus <evgeniy.jeka@gmail.com>
2021-09-02 21:52:13 +0300

    Creditto test framework - in progress

--- QaService/base_config.py
+++ QaService/base_config.py
@@ -1,4 +1,30 @@
 import configparser
+import os
+
+
+def get_parser(config):
+    parser = configparser.ConfigParser()
+    with open(config, mode='r', buffering=-1, closefd=True):
+        parser.read(config)
+        return parser
 
 class BaseConfig(object):
-    pass
+
+    config_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.ini')
+    parser = get_parser(config_file)
+    print(os.getcwd())
+
+    # Running locally
+    if os.getenv('BASE_URL') is None:
+        BASE_URL = parser.get('URL', 'base_url')
+        SQL_HOST = parser.get('SQL_DB', 'host')
+
+    # Running in Docker container
+    else:
+        BASE_URL = os.getenv('BASE_URL')
+        SQL_HOST = os.getenv('SQL_HOST')
+
+
+if __name__ == '__main__':
+    bc = BaseConfig()
+    print(bc.BASE_URL)
