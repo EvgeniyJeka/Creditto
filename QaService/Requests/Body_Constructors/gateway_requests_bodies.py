811c6952ab9782d2c3c00c5b0eab9325f3abd744
Prometeus <evgeniy.jeka@gmail.com>
2021-09-03 21:23:31 +0300

    Added request 'add bid'.

--- QaService/Requests/Body_Constructors/gateway_requests_bodies.py
+++ QaService/Requests/Body_Constructors/gateway_requests_bodies.py
@@ -7,15 +7,28 @@ import logging
 class GatewayRequestsBodies:
 
     @staticmethod
-    def place_offer_request_body(owner_id, sum, duration_months, offered_interest, allow_partial_fill):
+    def place_offer_request_body(owner_id, _sum, duration_months, offered_interest, allow_partial_fill):
 
         payload_composed = {
             "type": "offer",
             "owner_id": owner_id,
-            "sum": sum,
+            "sum": _sum,
             "duration": duration_months,
             "offered_interest": offered_interest,
             "allow_partial_fill": allow_partial_fill
+        }
+
+        return payload_composed
+
+    @staticmethod
+    def place_bid_request_body(owner_id, bid_interest, target_offer_id, partial_only):
+
+        payload_composed = {
+            "type": "bid",
+            "owner_id": owner_id,
+            "bid_interest": bid_interest,
+            "target_offer_id": target_offer_id,
+            "partial_only": partial_only
         }
 
         return payload_composed
--- QaService/Requests/gateway_requests.py
+++ QaService/Requests/gateway_requests.py
@@ -10,7 +10,7 @@ base_url = BaseConfig.BASE_URL
 
 class GatewayRequests(object):
 
-    def place_offer(self, owner_id, sum, duration_months, offered_interest, allow_partial_fill) -> json:
+    def place_offer(self, owner_id, _sum, duration_months, offered_interest, allow_partial_fill) -> json:
         """
         Sends HTTP POST request to Gateway in order to place a new Offer
         :return: Response body as a json.
@@ -18,7 +18,7 @@ class GatewayRequests(object):
 
         url = base_url + 'place_offer'
 
-        payload = GatewayRequestsBodies.place_offer_request_body(owner_id, sum, duration_months, offered_interest,
+        payload = GatewayRequestsBodies.place_offer_request_body(owner_id, _sum, duration_months, offered_interest,
                                                                  allow_partial_fill)
 
         try:
@@ -35,7 +35,32 @@ class GatewayRequests(object):
             logging.error(F"{e.__class__.__name__} place_offer failed with error: {e}")
             raise e
 
+    def place_bid(self, owner_id, bid_interest, target_offer_id, partial_only) -> json:
+        """
+        Sends HTTP POST request to Gateway in order to place a new Bid
+        :return: Response body as a json.
+        """
+
+        url = base_url + 'place_bid'
+
+        payload = GatewayRequestsBodies.place_bid_request_body(owner_id, bid_interest, target_offer_id, partial_only)
+
+        try:
+            response = requests.post(url, json=payload, timeout=BaseConfig.WAIT_BEFORE_TIMEOUT)
+            body = json.loads(response.text)
+            logging.info("Service Response: {0}".format(body))
+            return body
+
+        except json.decoder.JSONDecodeError as e:
+            logging.error(f"Failed to convert the response to JSON, response: {response}, text: {response.text}")
+            raise e
+
+        except Exception as e:
+            logging.error(F"{e.__class__.__name__} place_offer failed with error: {e}")
+            raise e
+
 
 if __name__ == '__main__':
     gr = GatewayRequests()
-    print(gr.place_offer("1021", 100000, 12, 0.08, 0 ))
+    #print(gr.place_offer("1021", 100000, 12, 0.08, 0 ))
+    print(gr.place_bid("223", 0.06, 5, 0))
