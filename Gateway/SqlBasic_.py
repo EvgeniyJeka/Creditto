a6e243838ebae0e0b62b8113acc09ba78e266335
Prometeus <evgeniy.jeka@gmail.com>
2022-04-10 18:12:07 +0300

    Gateway - replacing queries in existing SQL related methods with SQL Alchemy.
    Create/validate table - refactored.

--- Gateway/SqlBasic_.py
+++ Gateway/SqlBasic_.py
@@ -156,70 +156,100 @@ class SqlBasic(object):
             query = created_config_table.insert().values([*added_values])
             self.cursor.execute(query)
 
+    def get_columns(self, table):
+        """
+        Returns a list of column names
+        @param table: existing table, str
+        @return: list of str
+        """
+        try:
+            metadata = db.MetaData()
+            table_ = db.Table(table, metadata, autoload=True, autoload_with=self.engine)
+
+            return table_.columns.keys()
+
+        except Exception as e:
+            logging.error(f" Failed to fetch column names of table {table} - {e}")
+            return False
 
 
-            #         logging.warning("Logs: ADDING THE DEFAULT CONFIG")
-            #
-            #         query = f'insert into local_config values(1, "matching_logic", 1, "selected matching algorithm")'
-            #         cursor.execute(query)
-            #         query = f'insert into local_config values(2, "tail_digits", 4, "max tail digits allowed, rounding config")'
-            #         cursor.execute(query)
+    def get_table_content(self, table):
+        """
+        Get table content from DB
+        :param table: table name, String
+        :return: tuple
+        """
+        metadata = db.MetaData()
+        table_ = db.Table(table, metadata, autoload=True, autoload_with=self.engine)
+
+        query = db.select([table_])
+        ResultProxy = self.cursor.execute(query)
+        result = ResultProxy.fetchall()
+
+        return result
+
+    def get_offer_data(self, offer_id: int):
+        try:
+            metadata = db.MetaData()
+            table_ = db.Table("offers", metadata, autoload=True, autoload_with=self.engine)
+
+            query = db.select([table_]).where(table_.columns.id == offer_id)
+            ResultProxy = self.cursor.execute(query)
+            result = ResultProxy.fetchall()
+
+            return self.pack_to_dict(result, "offers")
+
+        except Exception as e:
+            logging.error(f"SQL Module: Failed to get offer data from SQL - {e}")
+
+
+    def pack_to_dict(self, data, table):
+        """
+        This method can be used to extract data from SQL table and pack it to list of dicts
+        @param query: query to execute
+        @param table: SQL table
+        @return: list of dicts
+        """
+        try:
+            columns = self.get_columns(table)
+
+            if data is None:
+                logging.warning(f"Couldn't find the requested data by provided param")
+                return []
+
+            elif len(data) == 1:
+                cnt = 0
+                result = {}
+                for column in columns:
+                    result[column] = data[0][cnt]
+                    cnt += 1
+                return [result]
+
+            result = []
+            for contact in data:
+                cnt = 0
+                record = {}
+                for column in columns:
+                    record[column] = contact[cnt]
+                    cnt += 1
+                result.append(record)
+            return result
+
+        except Exception as e:
+            logging.error(f"Failed to pack data from SQL, {e}")
+            raise e
 
 
 
 
-    # # Validates that all the required tables exist, if they aren't - the method creates them.
-    # def create_validate_tables(self, cursor):
-    #     """
-    #     This method can be used to validate, that all needed table are exist.
-    #     If they aren't the method will create them
-    #     :param cursor: sql cursor
-    #     """
-    #     cursor.execute('show tables')
-    #     tups = cursor.fetchall()
-    #
-    #     tables = [tup[0] for tup in tups]
-    #
-    #     # Creating the 'offers' table if not exists - column for each "Offer" object property.
-    #     if 'offers' not in tables:
-    #         logging.warning("Logs: 'offers' table is missing! Creating the 'offers' table")
-    #         query = "CREATE TABLE offers (id bigint, owner_id int, sum varchar(255), " \
-    #                 "duration int, offered_interest varchar(255), final_interest varchar(255), allow_partial_fill int, date_added varchar(255), " \
-    #                 "status int, PRIMARY KEY (ID));"
-    #
-    #         cursor.execute(query)
-    #
-    #     # Creating the 'bids' table if not exists - column for each "Bid" object property.
-    #     if 'bids' not in tables:
-    #         logging.warning("Logs: 'bids' table is missing! Creating the 'bids' table")
-    #         query = "CREATE TABLE bids (id bigint, owner_id int, bid_interest varchar(255), target_offer_id bigint, " \
-    #                 "partial_only int, date_added varchar(255), status int, PRIMARY KEY (ID));"
-    #
-    #         cursor.execute(query)
-    #
-    #     if 'matches' not in tables:
-    #         logging.warning("Logs: 'matches' table is missing! Creating the 'bids' table")
-    #         query = "CREATE TABLE matches (id bigint, offer_id bigint, bid_id bigint, offer_owner_id int, bid_owner_id int, " \
-    #                 "match_time varchar(255), partial int, sum varchar(255), final_interest varchar(255), " \
-    #                 "monthly_payment varchar(255)," \
-    #                 " PRIMARY KEY (ID));"
-    #
-    #         cursor.execute(query)
-    #
-    #     if 'local_config' not in tables:
-    #         logging.warning("Logs: 'local_config' table is missing! Creating the 'local_config' table")
-    #         query = "CREATE TABLE local_config (id bigint, property varchar(255), " \
-    #                 "value  varchar(255), description varchar(255), PRIMARY KEY (ID));"
-    #
-    #         cursor.execute(query)
-    #         logging.warning("Logs: ADDING THE DEFAULT CONFIG")
-    #
-    #         query = f'insert into local_config values(1, "matching_logic", 1, "selected matching algorithm")'
-    #         cursor.execute(query)
-    #         query = f'insert into local_config values(2, "tail_digits", 4, "max tail digits allowed, rounding config")'
-    #         cursor.execute(query)
-    #         logging.warning("Logs: SETTING THE DEFAULT CONFIG")
+
+
+
+
+
+
 
 if __name__ == '__main__':
     print("Test")
     sql_basic = SqlBasic()
+    print(sql_basic.get_offer_data(752781996402836800))
