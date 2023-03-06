af9ec6c3cb5acdf8cd926d4ee5e6354d745413f1
Prometeus <evgeniy.jeka@gmail.com>
2023-03-06 13:51:06 +0200

    Updating existing tests

---
+++ QaService/Tools/results_reporter.py
@@ -0,0 +1,44 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import logging
+
+
+class ResultsReporter:
+
+    def send_report_slack(self, data):
+        """
+        This method is used to send test run reports to the designated Slack channel.
+        Slack channel name and Slack token are taken from environment variables
+        :param data: report text
+        :return: True on success
+        """
+
+        client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+        channel_name = os.environ['SLACK_RESULTS_REPORTING_CHANNEL_NAME']
+
+        try:
+            response = client.chat_postMessage(
+                channel=channel_name,
+                text=data
+            )
+            logging.info(f"Test Result Published To Slack Channel Successfully: {response}")
+            return True
+
+        except SlackApiError as e:
+            logging.error("Error sending message: ", e)
+            raise e
+
+    def report_success(self, test_id, test_file_name):
+        report = f"Test PASSED : #{test_id} , {test_file_name}"
+        return self.send_report_slack(report)
+
+    def report_failure(self, test_id, test_file_name):
+        report = f"Test FAILED : #{test_id} , {test_file_name}"
+        return self.send_report_slack(report)
+
+    def report_broken_test(self, test_id, test_file_name, test_broken_exception):
+        report = f"Test BROKEN : #{test_id} , {test_file_name} - note the exception: {test_broken_exception}"
+        return self.send_report_slack(report)
+
+
